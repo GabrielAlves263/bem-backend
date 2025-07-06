f7dd6252cc4a4d57027039392e0cb960e274f21f
GabrielAlves263 <jgabrielalves@alu.ufc.br>
2025-07-06 07:32:38 -0300

    feat: Implement initial FastAPI application with Google Generative AI and ElevenLabs integration

---
+++ app.py
@@ -0,0 +1,62 @@
+import google.generativeai as genai
+from elevenlabs import ElevenLabs, save
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+genai.configure(api_key="AIzaSyB5qRu7RPEaj6gSNiuagVvX28VkN-eX9DI")
+elevenlabs = ElevenLabs(
+    api_key="sk_d2c0ca5df7924c1cb2dbe95cfb629f03742ee57c91b24e15",
+)
+
+modelo = genai.GenerativeModel("gemma-3n-e2b-it")
+
+app = FastAPI()
+
+
+def carregar_prompt(caminho="prompt.txt"):
+    with open(caminho, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+prompt = carregar_prompt()
+
+chat = modelo.start_chat(
+    history=[
+        {
+            "role": "user",
+            "parts": [prompt],
+        },
+    ]
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # To:do Colocar domínio Next.js
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Pergunta(BaseModel):
+    texto: str
+
+
+@app.post("/perguntar")
+def perguntar(dados: Pergunta):
+    resposta = chat.send_message(dados.texto)
+    return {"resposta": resposta.text}
+
+
+@app.post("/falar")
+def falar(dados: Pergunta):
+    audio = elevenlabs.text_to_speech.convert(
+        text=dados.texto,
+        voice_id="SAz9YHcvj6GT2YYXdXww",
+        model_id="eleven_multilingual_v2",
+        output_format="mp3_44100_128",
+    )
+    caminho = "resposta.mp3"
+    save(audio, caminho)
+    return FileResponse(caminho, media_type="audio/mpeg")
